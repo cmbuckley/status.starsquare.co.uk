4ea77baec939bfd0be64cc38d98df1254308187f
Chris Buckley <chris@cmbuckley.co.uk>
2024-11-24 18:02:56 +0000

    Add Jordan/Bluesky, better monitor names

--- src/config.ts
+++ src/config.ts
@@ -9,44 +9,53 @@ export const config: Config = {
   },
   monitors: [{
     id: 'bux-re',
-    name: 'bux.re',
+    name: 'Bux Short URL',
     url: 'https://bux.re',
 	expectStatus: 302,
   }, {
     id: 'cmbuckley-co-uk',
-    name: 'cmbuckley.co.uk',
+    name: 'Chris Personal Site',
     url: 'https://cmbuckley.co.uk/',
   }, {
+    id: 'cmbuckley-me',
+    name: 'Bluesky',
+    url: 'https://cmbuckley.me/',
+	expectStatus: 302,
+  }, {
     id: 'emilia-buckley-name',
-    name: 'emilia.buckley.name',
+    name: 'Emilia Buckley',
     url: 'https://emilia.buckley.name',
   }, {
-    id: 'emilia-chris-wedding',
-    name: 'emilia-chris-wedding.starsquare.co.uk',
+    id: 'emilia-chris-wedding-starsquare-co-uk',
+    name: 'Emilia & Chris Wedding',
     url: 'https://emilia-chris-wedding.starsquare.co.uk',
   }, {
     id: 'icorrect-it',
-    name: 'icorrect.it',
+    name: 'I Correct It',
     url: 'https://icorrect.it',
   }, {
+    id: 'jordanhume-co-uk',
+    name: 'Jordan Hume',
+    url: 'https://jordanhume.co.uk',
+  }, {
     id: 'misterf-co-uk',
-    name: 'misterf.co.uk',
+    name: 'Mister F',
     url: 'https://misterf.co.uk',
   }, {
     id: 'setcookie-net',
-    name: 'setcookie.net',
+    name: 'Set Cookie',
     url: 'https://setcookie.net/',
   }, {
     id: 'scripts-cmbuckley-co-uk',
-    name: 'scripts.cmbuckley.co.uk',
+    name: 'Scripts',
     url: 'https://scripts.cmbuckley.co.uk/',
   }, {
     id: 'starsquare-co-uk',
-    name: 'starsquare.co.uk',
+    name: 'StarSquare Designs',
     url: 'https://starsquare.co.uk',
   }, {
     id: 'thesparesshop-co-uk',
-    name: 'thesparesshop.co.uk',
+    name: 'The Spares Shop',
     url: 'https://thesparesshop.co.uk',
   }, {
     id: 'vistaverde-network-home-assistant',
@@ -54,7 +63,7 @@ export const config: Config = {
     url: 'https://pi.vistaverde.network',
   }, {
     id: 'yorkstudenthomes-com',
-    name: 'yorkstudenthomes.com',
+    name: 'York Student Homes',
     url: 'https://yorkstudenthomes.com',
   }],
 }

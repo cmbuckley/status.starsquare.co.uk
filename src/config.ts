34621bdc5adeeec820f604cb015211f66ca0158c
Chris Buckley <chris@cmbuckley.co.uk>
2024-11-24 10:08:10 +0000

    Remove remote monitors

--- src/config.ts
+++ src/config.ts
@@ -57,5 +57,4 @@ export const config: Config = {
     name: 'yorkstudenthomes.com',
     url: 'https://yorkstudenthomes.com',
   }],
-  monitorsCsvUrl: 'https://docs.google.com/spreadsheets/d/e/2PACX-1vSnewwW9OuXgtuutyYSfFJ_AZdI-UpkUjP2wWi-zZWM3MKa8IzBceWCe9qB_-Lmk-S7mSFgqKVnokam/pub?gid=0&single=true&output=csv',
 }

19ab5e5998c05457988b151d2d634f80f801c0a5
Chris Buckley <chris@cmbuckley.co.uk>
2024-11-24 15:28:07 +0000

    add secrets to dev vars
    causes `wrangler types` to add them to Env

--- src/shims.d.ts
+++ src/shims.d.ts
@@ -1,10 +1,5 @@
 export {}
 declare global {
-  const SECRET_SLACK_WEBHOOK_URL: string
-  const SECRET_TELEGRAM_CHAT_ID: string | undefined
-  const SECRET_TELEGRAM_API_TOKEN: string | undefined
-  const SECRET_DISCORD_WEBHOOK_URL: string
-
   namespace Vike {
     interface PageContext {
       env: Env
